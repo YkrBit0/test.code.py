6ea16ed1118af4e77f99422bc072d5d90c48093f
YkrBit0 <83524483+YkrBit0@users.noreply.github.com>
2021-05-27 22:11:23 +0800

    added a file

---
+++ pick_time.py
@@ -0,0 +1,37 @@
+# -*- codeing =utf-8 -*-
+# @Time : 2021/5/26 11:16
+# @Author : 小洛
+# @File : pick_time.py
+# @Software: PyCharm
+
+
+import pickle
+
+d = {
+    "name":"alex",
+    "role":"police",
+    "blood":76,
+    "weapon":"Ak47"
+}
+#alive_players = ["alex","jack","rain"]
+
+#日期
+import datetime
+#pickle  支持pyhon的所有数据类型
+#t = pickle.dumps(datetime.datetime.now())
+
+#print(t)
+
+#print(pickle.loads(t))
+
+
+# import json
+#报错----json---不支持数据类型
+# t1 = json.dumps(datetime.datetime.now())
+#
+# print(t1)
+
+
+
+
+
